6e2de1485cc0e3a8e5881c9e7dd32da668416aa7
Ian Pierce <iandpierce@gmail.com>
2019-11-03 23:27:03 -0600

    header text is highlighted corresponding to route

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 
-export default function Header() {
+export default function Header({match, location}) {
     return (
         <nav className="navigation__container">
             <ul>
                 <li>
-                    <Link to="/">
+                    <Link className={'icon'} to="/">
                         <div className="navigation__header-icon"></div>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/events">Events</Link>
+                    <Link className={'text-link-header ' + (location.pathname === '/events' ? 'selected' : '')} to="/events">EVENTS</Link>
                 </li>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link className={'text-link-header ' + (location.pathname === '/' ? 'selected' : '')} to="/">HOME</Link>
                 </li>
                 <li>
-                    <Link to="/plex">Plex</Link>
+                    <Link className={'text-link-header ' + (location.pathname === '/plex' ? 'selected' : '')} to="/plex">PLEX</Link>
                 </li>
             </ul>
         </nav>
--- src/index.js
+++ src/index.js
@@ -2,26 +2,34 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-import './styles/app.scss';
 import Events from './components/pages/events'
 import Header from './components/header'
 import Home from './components/pages/home'
 import PageContentContainer from "./components/pages/page-content-container";
 import Plex from "./components/pages/plex";
 
-function App() {
-    return (
-        <Router>
-            <Route path="/" component={Header} />
-            <PageContentContainer>
-                <Switch>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/events" component={() => <Events />} />
-                    <Route path="/plex" component={Plex} />
-                </Switch>
-            </PageContentContainer>
-        </Router>
-    );
+import './styles/app.scss';
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <Router>
+                <Route component={ Header }/>
+                <PageContentContainer>
+                    <Switch>
+                        <Route exact path="/" component={ Home } />
+                        <Route path="/events" component={ Events } />
+                        <Route path="/plex" component={ Plex } />
+                    </Switch>
+                </PageContentContainer>
+            </Router>
+        );
+    }
+
 }
 
 ReactDOM.render(
